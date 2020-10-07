b9d811b9cfd2d0b9b003b8c7dd494f87acc4c9bf
Volodymyr77 <gagarinvov@gmail.com>
2020-10-07 20:36:16 +0200

    secondInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tryToPush';
+
+  // getTitle(yyy: HTMLInputElement): void {
+  //   console.log(this.title);
+  //   console.log(yyy);
+  //   console.log(yyy.value);
+
+  getTitle(yyy: NgModel): void {
+    console.log(yyy);
+
+  }
+
 }
